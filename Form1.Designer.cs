f0481523578785b9b9f3d352d2128f102d4effd7
Duygu Tümer <69381839+duygutumer@users.noreply.github.com>
2023-11-01 23:31:41 +0300

    connect and send message

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,8 @@ namespace WinFormsApp1
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // client
@@ -52,29 +53,29 @@ namespace WinFormsApp1
             this.client.Size = new System.Drawing.Size(187, 81);
             this.client.TabIndex = 0;
             this.client.Text = "Client";
-            this.client.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 470);
+            this.textBox1.Location = new System.Drawing.Point(60, 628);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(772, 164);
+            this.textBox1.Size = new System.Drawing.Size(879, 164);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(677, 653);
+            this.button1.Location = new System.Drawing.Point(794, 823);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 54);
             this.button1.TabIndex = 2;
             this.button1.Text = "Send\r\n";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Location = new System.Drawing.Point(666, 34);
+            this.button2.Location = new System.Drawing.Point(773, 34);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 70);
             this.button2.TabIndex = 3;
@@ -88,12 +89,11 @@ namespace WinFormsApp1
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 41);
             this.label1.TabIndex = 4;
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 413);
+            this.label2.Location = new System.Drawing.Point(60, 571);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 41);
             this.label2.TabIndex = 5;
@@ -109,7 +109,7 @@ namespace WinFormsApp1
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(143, 244);
+            this.textBox3.Location = new System.Drawing.Point(507, 176);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(239, 47);
             this.textBox3.TabIndex = 7;
@@ -118,7 +118,7 @@ namespace WinFormsApp1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 244);
+            this.label3.Location = new System.Drawing.Point(414, 176);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 41);
             this.label3.TabIndex = 8;
@@ -134,22 +134,31 @@ namespace WinFormsApp1
             this.label4.Text = "Host:\r\n";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // button3
+            // connect_btn
             // 
-            this.button3.Location = new System.Drawing.Point(50, 325);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 54);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Connect\r\n\r\n";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.connect_btn.Location = new System.Drawing.Point(784, 172);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(145, 54);
+            this.connect_btn.TabIndex = 10;
+            this.connect_btn.Text = "Connect\r\n\r\n";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(60, 281);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(869, 233);
+            this.textBox4.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(874, 728);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1009, 913);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.connect_btn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -179,7 +188,8 @@ namespace WinFormsApp1
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,34 +7,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        bool terminating = false;
+        bool connected = false;
+
+        Socket clientSocket;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox2.Text;
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            int portNum;
+            if (Int32.TryParse(textBox3.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    textBox4.AppendText("Connected to the server!\n");
+                    connected = true;
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start();
 
+                }
+                catch
+                {
+                    textBox4.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                textBox4.AppendText("Check the port\n");
+            }
+        }
+        private void Receive()
+        {
+            while (connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    textBox4.AppendText("Server: " + incomingMessage + "\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        textBox4.AppendText("The server has disconnected\n");
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+
+            }
+        }
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+            terminating = true;
+            Environment.Exit(0);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = textBox1.Text;
+
+            if (message != "" && message.Length <= 64)
+            {
+                Byte[] buffer = new Byte[64];
+                buffer = Encoding.Default.GetBytes(message);
+                clientSocket.Send(buffer);
+            }
         }
     }
 }
